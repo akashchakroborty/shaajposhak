e4f077df45200a491e767cca3fb557948eb5c888
akashchakroborty <akashranjanchakroborty@gmail.com>
2019-10-09 22:25:59 +0530

    added checkout page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import Checkout from "./pages/checkout/checkout.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "./redux/user/user.selector";
@@ -56,6 +57,7 @@ class App extends Component {
               )
             }
           />
+          <Route exact path="/checkout" component={Checkout} />
         </Switch>
       </div>
     );

d033e820c77d231b2462e59ad7d309dce5381825
akash chakroborty <akashranjanchakroborty@gmail.com>
2019-10-12 14:13:21 +0530

    changing shop data to hash map table and completing collection component

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./collection-overview.styles.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../redux/shop/shop.selector";
+import { selectShopCollectionsForPreview } from "../../redux/shop/shop.selector";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
 const CollectionOverview = ({ collections }) => {
@@ -16,7 +16,7 @@ const CollectionOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
+  collections: selectShopCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);

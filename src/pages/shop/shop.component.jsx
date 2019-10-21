6b9b2beec57ab5d7e630513e424022f614e4d0c0
akash chakroborty <akashranjanchakroborty@gmail.com>
2019-10-21 21:45:14 +0530

    Convert Routes components into Lazy loaded components with React Lazy and Wrapping them inside Suspense

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
-import CollectionOverviewContainer from "../../components/collection-overview/collection-overview.container";
-import CollectionContainer from "./../collection/collection.container";
 import ShopActionTypes from "../../redux/shop/shop.types";
+import Spinner from "../../components/spinner/spinner.component";
+
+const CollectionOverviewContainer = lazy(() =>
+  import("../../components/collection-overview/collection-overview.container")
+);
+const CollectionContainer = lazy(() =>
+  import("./../collection/collection.container")
+);
 
 const ShopPage = ({ match, dispatch }) => {
   useEffect(() => {
@@ -14,15 +20,17 @@ const ShopPage = ({ match, dispatch }) => {
 
   return (
     <div className="shop-page">
-      <Route
-        exact
-        path={`${match.path}`}
-        component={CollectionOverviewContainer}
-      />
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionContainer}
-      />
+      <Suspense fallback={Spinner}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionContainer}
+        />
+      </Suspense>
     </div>
   );
 };

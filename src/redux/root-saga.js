54f3f5a95bf34aac8d10d64b00a7072e8f9cdbc6
akash chakroborty <akashranjanchakroborty@gmail.com>
2020-01-09 23:02:53 +0530

    server.js changes

--- src/redux/root-saga.js
+++
@@ -1,8 +0,0 @@
-import { all, call } from "redux-saga/effects";
-import { shopSagas } from "../redux/shop/shop.sagas";
-import { userSagas } from "../redux/user/user.saga";
-import { cartSagas } from "../redux/cart/cart.saga";
-
-export default function* rootSaga() {
-  yield all([call(shopSagas), call(userSagas), call(cartSagas)]);
-}
